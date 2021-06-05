3cd25befab9e430a481f8816c56c09861d04f556
tamanaya876 <76041889+tamanaya876@users.noreply.github.com>
2021-06-05 14:50:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 img = "";
 
+status = "";
+
 function preload()
 {
     img = loadImage('dog_cat.jpg');
@@ -7,16 +9,45 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(550,420);
+    canvas = createCanvas(550,370);
     canvas.center();
+
+    objectDetection = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Object Detection";
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+    status = true;
+    objectDetection.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{    
+    if(error)
+    {
+        console.error(error);
+    }
+
+    else
+    {
+        console.log(results);
+    }
 }
 
 function draw()
 {
-    image(img,0,0,550,420);
+    image(img,0,0,550,370);
     fill("#FF0000");
     text("Dog",45,75);
     noFill();
     stroke("#FF0000");
     rect(30,60,450,350);
+
+    fill("#FF0000");
+    text("Cat",270,81)
+    noFill();
+    stroke("#FF0000");
+    rect(260,70,250,350);
 }
